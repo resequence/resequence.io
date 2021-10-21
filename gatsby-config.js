1dc1fcd9fb93d9f6356339663d8ff1e7719ae2ba
Matthew Campbell <mjcampbell@vmware.com>
2021-10-21 18:00:33 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "resequence.io",
+  },
+  plugins: ["gatsby-plugin-react-helmet", "gatsby-plugin-sitemap"],
+};
