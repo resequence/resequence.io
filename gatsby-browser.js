583bcbd2dbb1364134fccc6a4603301c2169c70d
Matthew Campbell <mjcampbell@vmware.com>
2021-10-21 20:15:39 -0400

    initial landing page

---
+++ gatsby-browser.js
@@ -0,0 +1,6 @@
+import "@fontsource/nunito-sans/400.css"
+import "@fontsource/nunito-sans/800.css"
+import "@fontsource/nunito/400.css"
+import "@fontsource/nunito/800.css"
+
+import "./src/styles/global.css"
